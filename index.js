d148beef5e97ad982bb136f924b82b6b48ee3a46
Biplob <119670070+biplobDlab@users.noreply.github.com>
2024-11-08 16:04:26 +0600

    lowercase chage

--- index.js
+++ index.js
@@ -71,51 +71,47 @@ function getAllUrlParams(url) {
     // if query string exists
     if (queryString) {
   
-      // stuff after # is not part of query string, so get rid of it
+      // remove stuff after #, as it’s not part of the query string
       queryString = queryString.split('#')[0];
   
-      // split our query string into its component parts
+      // split the query string into component parts
       var arr = queryString.split('&');
   
       for (var i = 0; i < arr.length; i++) {
-        // separate the keys and the values
+        // separate the keys and values
         var a = arr[i].split('=');
   
         // set parameter name and value (use 'true' if empty)
         var paramName = a[0];
         var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
   
-        // (optional) keep case consistent
-        paramName = paramName.toLowerCase();
-        if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
-  
-        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+        // if the paramName ends with square brackets, e.g., colors[] or colors[2]
         if (paramName.match(/\[(\d+)?\]$/)) {
   
           // create key if it doesn't exist
           var key = paramName.replace(/\[(\d+)?\]/, '');
           if (!obj[key]) obj[key] = [];
   
-          // if it's an indexed array e.g. colors[2]
+          // if it's an indexed array, e.g., colors[2]
           if (paramName.match(/\[\d+\]$/)) {
-            // get the index value and add the entry at the appropriate position
+            // get the index value and add the entry at the specified position
             var index = /\[(\d+)\]/.exec(paramName)[1];
             obj[key][index] = paramValue;
           } else {
-            // otherwise add the value to the end of the array
+            // otherwise, add the value to the end of the array
             obj[key].push(paramValue);
           }
         } else {
-          // we're dealing with a string
+          // handling string parameters
           if (!obj[paramName]) {
             // if it doesn't exist, create property
             obj[paramName] = paramValue;
           } else if (obj[paramName] && typeof obj[paramName] === 'string'){
-            // if property does exist and it's a string, convert it to an array
+            // if the property exists as a string, convert it to an array
             obj[paramName] = [obj[paramName]];
             obj[paramName].push(paramValue);
           } else {
-            // otherwise add the property
+            // otherwise, add the property
             obj[paramName].push(paramValue);
           }
         }
@@ -123,7 +119,8 @@ function getAllUrlParams(url) {
     }
   
     return obj;
-  }
+}
+
 
 
 console.log(getAllUrlParams(window.location.href))
