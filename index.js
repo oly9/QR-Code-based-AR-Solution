3d44511d10ec1b7613b979e66f27c7da174a4f2c
Biplob <119670070+biplobDlab@users.noreply.github.com>
2024-11-10 07:26:43 +0600

    testing with new pattern

--- index.js
+++ index.js
@@ -21,18 +21,6 @@ marker.addEventListener("markerLost", () => {
     document.getElementById("video_assect").pause();
 });
 
-
-
-// const d_marker = document.querySelector("#d_marker");
-// d_marker.addEventListener("markerFound", () => {
-//     $(scanUI).hide();
-//     document.getElementById("video_assect_server").play();
-// });
-// d_marker.addEventListener("markerLost", () => {
-//     $(scanUI).show();
-//     document.getElementById("video_assect_server").pause();
-// });
-
 ////////////////////////////////////////// API INVOCKING //////////////////////////////////
 
 
@@ -45,75 +33,13 @@ const patternParam = urlObj.searchParams.get("pattern");
 
 console.log("Video:", videoParam)
 
-// function getAllUrlParams(url) {
-
-//     // get query string from url (optional) or window
-//     var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-//     // we'll store the parameters here
-//     var obj = {};
-
-//     // if query string exists
-//     if (queryString) {
-
-//         // remove stuff after #, as it’s not part of the query string
-//         queryString = queryString.split('#')[0];
-
-//         // split the query string into component parts
-//         var arr = queryString.split('&');
-
-//         for (var i = 0; i < arr.length; i++) {
-//             // separate the keys and values
-//             var a = arr[i].split('=');
-
-//             // set parameter name and value (use 'true' if empty)
-//             var paramName = a[0];
-//             var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-//             // if the paramName ends with square brackets, e.g., colors[] or colors[2]
-//             if (paramName.match(/\[(\d+)?\]$/)) {
-
-//                 // create key if it doesn't exist
-//                 var key = paramName.replace(/\[(\d+)?\]/, '');
-//                 if (!obj[key]) obj[key] = [];
-
-//                 // if it's an indexed array, e.g., colors[2]
-//                 if (paramName.match(/\[\d+\]$/)) {
-//                     // get the index value and add the entry at the specified position
-//                     var index = /\[(\d+)\]/.exec(paramName)[1];
-//                     obj[key][index] = paramValue;
-//                 } else {
-//                     // otherwise, add the value to the end of the array
-//                     obj[key].push(paramValue);
-//                 }
-//             } else {
-//                 // handling string parameters
-//                 if (!obj[paramName]) {
-//                     // if it doesn't exist, create property
-//                     obj[paramName] = paramValue;
-//                 } else if (obj[paramName] && typeof obj[paramName] === 'string') {
-//                     // if the property exists as a string, convert it to an array
-//                     obj[paramName] = [obj[paramName]];
-//                     obj[paramName].push(paramValue);
-//                 } else {
-//                     // otherwise, add the property
-//                     obj[paramName].push(paramValue);
-//                 }
-//             }
-//         }
-//     }
-
-//     return obj;
-// }
-
-//var sourceData = getAllUrlParams(window.location.href)
 
 var patternUrl = fatchdataURL + patternParam
 var videoUrl = fatchdataURL + videoParam
 
 const words = patternParam.split('/');
 const id = words[0]
-console.log("ID: "+words[0])
+console.log("ID: "+id)
 
 function getDeviceType() {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
@@ -137,7 +63,7 @@ window.addEventListener("load", function () {
 
     // Data to send to the API
     const data = {
-        id: id,//"IbX3YGm", // replace this with a dynamic user or session ID if needed
+        id: id,                //"IbX3YGm", // replace this with a dynamic user or session ID if needed
         deviceName: deviceType
     };
 
@@ -157,7 +83,7 @@ window.addEventListener("load", function () {
 });
 
 
-
+////////////////////////////////////////////// adding dynamic /////////////////////////////////////////////
 
 //console.log(getAllUrlParams(window.location.href))
 
@@ -210,20 +136,6 @@ async function loadFiles() {
         const videoBlob = await videoResponse.blob();
         const videoBlobUrl = URL.createObjectURL(videoBlob);
 
-
-
-        // const marker = document.querySelector('#marker');
-        // marker.setAttribute('url', markerBlobUrl);
-
-        // Change the source of the existing video
-        // const video = document.querySelector('#video_assect');
-        // video.setAttribute('src', videoBlobUrl);
-
-
-
-        // Call the rerender function when needed
-        //rerenderScene();
-
         // Append the marker and video elements to the A-Frame scene
         $(assets).append(`
             <video id="dynamic_video_assect" src="${videoBlobUrl}" autoplay="" loop="true"></video>
@@ -249,42 +161,3 @@ async function loadFiles() {
         console.error("Error loading files:", error);
     }
 }
-
-// Call the function to load and display the files
-//loadFiles();
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////// NOT IN THE SCOPE  ////////////////////////////////////////
-// var videoLink = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-
-// // Create and configure the video element
-// const videoAsset = document.createElement('video');
-// videoAsset.id = 'dynamic-video';
-// videoAsset.src = videoLink;
-// videoAsset.autoplay = false; // Autoplay is set to false initially
-// videoAsset.loop = true;
-// videoAsset.setAttribute('webkit-playsinline', '');
-// videoAsset.setAttribute('playsinline', '');
-
-// $(videoplayer).append(videoAsset);
-
-// $('#player').attr('src', "value");
-// marker.addEventListener('markerFound', () => {
-//     scanUI.style.opacity = '0'; // Fade out scan UI
-//     videoAsset.play();
-//   });
-
-//   marker.addEventListener('markerLost', () => {
-//     videoAsset.pause();
-//   });
-//download marker
-// download video
-// need marker to be loded
-// need video to be loaded
-// need identify deveice
-// Invok API
